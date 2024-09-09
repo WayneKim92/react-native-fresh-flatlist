d9b3d92c03408c270aa529af62b743deed4bfc0e
WayneKim92 <eunchan0817@gmail.com>
2024-09-09 22:24:35 +0900

    chore: 임시 저장

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,14 +1,83 @@
-import { SafeAreaView, StyleSheet, View } from 'react-native';
-import FreshFlatList from 'react-native-fresh-flatlist'; // 경로를 실제 파일 경로로 수정하세요
+import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import FreshFlatList, {
+  type FetchMeta,
+  type FetchType,
+} from 'react-native-fresh-flatlist';
+import config from './ignore/config.json';
+import { type Board } from './ignore/types';
 
 export default function App() {
+  // 네트워크 로직은 어떻게 입력할 것인가?
+  const fetchList = async ({ fetchType, fetchPage }: FetchMeta) => {
+    let page = 1;
+    if (fetchType === 'first') {
+      page = 1;
+    }
+    if (fetchType === 'next') {
+      page = fetchPage + 1;
+    }
+
+    const response = await fetch(
+      `${config.api}boards?contentType=BOARD&ownerId=3&category=ALL&page=${page}&size=60&sort=createdAt`
+    );
+
+    const data = await response.json();
+
+    let list = null;
+    if (data && data.boardList) {
+      list = data.boardList;
+    }
+
+    return {
+      fetchType: 'current' as FetchType,
+      list: list as Board[],
+    };
+  };
+
+  console.log('렌더링 in App.tsx');
+
   return (
+    // 외부에서 처리할 연산, 내부에서 처리할 연산을 어떻게 구분하는 게 좋을까?
     <SafeAreaView style={styles.container}>
-      <FreshFlatList<{ a?: number; b?: number }>
-        initData={[{ a: 1 }, { b: 2 }]}
+      <FreshFlatList<Board>
+        fetchList={fetchList}
         renderItem={({ item }) => {
-          console.log(item);
-          return <View style={styles.box} />;
+          return (
+            <View style={{ backgroundColor: 'gray', gap: 8, padding: 12 }}>
+              <View style={{ flexDirection: 'row' }}>
+                <Image
+                  source={{ uri: item.writerImage }}
+                  style={{ width: 50, height: 50, borderRadius: 25 }}
+                />
+                <View style={{ paddingLeft: 8 }}>
+                  <Text>{item.writerSpaceName}</Text>
+                  <Text>@{item.writerHandle}</Text>
+                </View>
+              </View>
+
+              <View
+                style={{
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                }}
+              >
+                <Text>{item.content}</Text>
+
+                {item.imageList && item.imageList.length > 0 && (
+                  <Image
+                    source={{ uri: item.imageList[0] }}
+                    style={{ width: 75, height: 75, borderRadius: 8 }}
+                  />
+                )}
+              </View>
+            </View>
+          );
+        }}
+        style={{ flex: 1 }}
+        contentContainerStyle={{
+          flexGrow: 1,
+          paddingHorizontal: 16,
+          gap: 12,
         }}
       />
     </SafeAreaView>
@@ -18,13 +87,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   box: {
     width: 50,
     height: 50,
     marginVertical: 20,
     backgroundColor: 'red',
+    flexDirection: 'row',
+    alignSelf: 'center',
   },
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,87 @@
 import { FlatList, type FlatListProps } from 'react-native';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
-interface ExtendedFlatListProps<T> extends FlatListProps<T> {
-  initData: T[];
+// 남득할만한 interface
+// 내 기계는 알아서 딱, 센스 있게 딱!
+
+// 입력 받을 수 있는 props는 오직 하나
+// fetchList: (currentPage) => Promise<number>;
+// initData: 초기에 fetch 없이 list를 보여주고 싶을 때
+
+/**
+ * FreshTrigger
+ * - general: 일반적인 상황에서 fetch를 호출한다.
+ * - viewed-page: 페이지 이동 후 되돌아 왔을 때에도 fetch하여 해당 페이지를 갱신한다
+ */
+type FreshTrigger = 'general' | 'viewed-page' | string;
+export type FetchType = 'first' | 'previous' | 'current' | 'next';
+export type FetchMeta = {
+  fetchType: FetchType;
+  fetchPage: number;
+};
+interface FreshFlatListProps<T> extends Omit<FlatListProps<T>, 'data'> {
+  freshTriggers?: FreshTrigger[];
+  isFocused?: boolean;
+  fetchList: ({ fetchPage, fetchType }: FetchMeta) => Promise<{
+    fetchType: FetchType;
+    list: T[];
+  }>;
 }
 
-const FreshFlatList = <T,>(props: Omit<ExtendedFlatListProps<T>, 'data'>) => {
-  const { initData, renderItem } = props;
+const FreshFlatList = <T,>(props: FreshFlatListProps<T>) => {
+  const { renderItem, fetchList, ...otherProps } = props;
 
-  const [data, _setData] = useState<T[]>(initData);
+  const [data, setData] = useState<T[]>([]);
+  const [currentFetchType] = useState<FetchType>('first');
+  const currentPageRef = useRef(1);
 
-  return <FlatList<T> data={data} renderItem={renderItem} />;
+  useEffect(() => {
+    (async () => {
+      console.log('fetch 전', {
+        currentPage: currentPageRef.current,
+        currentFetchType,
+      });
+      const { list, fetchType } = await fetchList({
+        fetchPage: currentPageRef.current,
+        fetchType: currentFetchType,
+      });
+
+      console.log('fetch 후', {
+        currentPage: currentPageRef.current,
+        fetchType,
+        listLength: list.length,
+      });
+      setData(list);
+    })();
+  }, [currentFetchType, fetchList]);
+
+  console.log('렌더링 in FreshFlatList.tsx');
+
+  return (
+    <FlatList<T>
+      data={data}
+      renderItem={renderItem}
+      onViewableItemsChanged={(_info) => {
+        console.log('onViewableItemsChanged');
+      }}
+      onEndReachedThreshold={0.5}
+      onEndReached={({ distanceFromEnd }) => {
+        if (distanceFromEnd === 0) {
+          return;
+        }
+        console.log('onEndReached');
+      }}
+      contentContainerStyle={{ flexGrow: 1 }}
+      {...otherProps}
+    />
+  );
 };
+
+/*
+페이지 기반 useCase 나열하자
+- 스크롤 끝에 도달하여 다음 페이지를 호출하고 렌더링 해야한다.
+- 스크롤 최상단에서 당겨서 내리면 list를 다 지우고 새로운 1페이잔 렌더링 한다.
+- 리스트 항목의 상세로 이동하였다가 다시 리스트로 돌아오면, 현재 페이지만 fetch하고 리스트를 렌더링 한다.
+ */
 
 export default FreshFlatList;
