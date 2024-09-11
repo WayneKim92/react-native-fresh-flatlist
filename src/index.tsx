f69524d2b6a0167e56b7bd441486f27adf5ee655
WayneKim92 <eunchan0817@gmail.com>
2024-09-11 11:49:28 +0900

    refactor: 임시로 추가했던 주석들 제거

--- src/index.tsx
+++ src/index.tsx
@@ -11,28 +11,17 @@ import {
 import { useDevLog } from './hooks/useDevLog';
 import type { ViewToken } from '@react-native/virtualized-lists';
 
-// 남득할만한 interface
-// 내 기계는 알아서 딱, 센스 있게 딱!
-
-// 입력 받을 수 있는 props는 오직 하나
-// fetchList: (currentPage) => Promise<number>;
-// initData: 초기에 fetch 없이 list를 보여주고 싶을 때
-
-// RN 타입 중 export 안 되어 있는 타입만 여기에 나열하자
+// List of RN types that are not exported from the RN package
 type onEndReachedParam = { distanceFromEnd: number };
 type onViewableItemsChangedParam<T> = {
   viewableItems: Array<ViewToken<T>>;
   changed: Array<ViewToken<T>>;
 };
 
-// 상수 목록
+// list of constants
 const FIRST_PAGE = 1;
-/**
- * FreshTrigger
- * - general: 일반적인 상황에서 fetch를 호출한다.
- * - viewed-page: 페이지 이동 후 되돌아 왔을 때에도 fetch하여 해당 페이지를 갱신한다
- */
-type FreshTrigger = 'general' | 'viewed-page' | string;
+
+// Type of FreshFlatList
 export type FetchType = 'first' | 'current' | 'end-reached';
 export type FetchInputMeta<T> = {
   fetchType: FetchType;
@@ -46,7 +35,6 @@ export type FetchOutputMeta<T> = Promise<{
 
 interface FreshFlatListProps<T>
   extends Omit<FlatListProps<T>, 'data' | 'onEndReached' | 'keyExtractor'> {
-  freshTriggers?: FreshTrigger[];
   isFocused?: boolean;
   fetchList: (fetchInputMeta: FetchInputMeta<T>) => FetchOutputMeta<T>;
   devMode?: boolean;
@@ -55,6 +43,7 @@ export interface FreshFlatListRef {
   reset: () => void;
 }
 
+// Define FreshFlatList
 function FreshFlatList<T>(
   props: FreshFlatListProps<T>,
   ref: ForwardedRef<FreshFlatListRef>
@@ -264,10 +253,4 @@ export default forwardRef(FreshFlatList) as <T>(
   props: FreshFlatListProps<T> & { ref?: ForwardedRef<FreshFlatListRef> }
 ) => ReturnType<typeof FreshFlatList>;
 
-/*
-페이지 기반 useCase 나열하자
-- 스크롤 끝에 도달하여 다음 페이지를 호출하고 렌더링 해야한다.
-- 스크롤 최상단에서 당겨서 내리면 list를 다 지우고 새로운 1페이잔 렌더링 한다.
-- 리스트 항목의 상세로 이동하였다가 다시 리스트로 돌아오면, 현재 페이지만 fetch하고 리스트를 렌더링 한다.
- */
 export { useDevLog } from './hooks/useDevLog';
