70186742b122b586e6cf8bec364573a45890546f
Wayne <75321423+WayneKim92@users.noreply.github.com>
2024-09-11 13:36:17 +0900

    [Feat] refresh featrue v1 (#2)
    * chore: fetch timeout 처리 로직 추가
    
    * chore: react-navigation 준비
    
    * docs: useDevLog 훅 주석 추가
    
    * chore: 필요 없는 주석 제거
    
    * chore: 세부 스크린 추가
    
    * feat: 스크린 포커스가 다시 되었을 때 현재 스크린만 데이터만 refresh
    
    * feat: 현재 보고 있는 페이지를 갱신하여 렌더링하기
    
    * feat: refresh 데이터로 보일 수 있게 keyExtractor 구현
    
    * feat: refresh 데이터로 보일 수 있게 keyExtractor 구현
    
    * feat: refresh 데이터로 보일 수 있게 keyExtractor 구현
    
    * refactor: 임시로 추가했던 주석들 제거
    
    * refactor: 조금 더 문맥 파악이 쉬운 변수 이름 사용
    
    * feat: 현재 조작이 일어나고 있는 항목의 페이지 새로 고침

---
+++ example/src/screens/ListScreen.tsx
@@ -0,0 +1,183 @@
+import { useEffect, useRef, useState } from 'react';
+import FreshFlatList, {
+  type FreshFlatListRef,
+  useDevLog,
+} from 'react-native-fresh-flatlist';
+import {
+  Button,
+  Image,
+  Pressable,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import TestInputText from '../components/TestInputText';
+import type { Board, RootStackParamList } from '../types';
+import { fetchWithTimeout } from '../utils/functions';
+import config from '../ignore/config.json';
+import {
+  useNavigation,
+  type NavigationProp,
+  useIsFocused,
+} from '@react-navigation/native';
+
+export default function ListScreen() {
+  const [category, setCategory] = useState('ALL');
+  const [size, setSize] = useState(10);
+  const [ownerId, setOwnerId] = useState(29);
+  const freshFlatListRef = useRef<FreshFlatListRef>(null);
+  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+  const isFocused = useIsFocused();
+
+  const devLog = useDevLog(__DEV__);
+
+  const goToDetailScreen = (item: Board) => {
+    navigation.navigate('DetailScreen', { item });
+  };
+
+  useEffect(() => {
+    freshFlatListRef.current?.reset();
+  }, [ownerId]);
+
+  return (
+    // 외부에서 처리할 연산, 내부에서 처리할 연산을 어떻게 구분하는 게 좋을까?
+    <SafeAreaView style={styles.container}>
+      <View style={{ paddingHorizontal: 16, paddingVertical: 12 }}>
+        <TestInputText
+          label={'Owner Id'}
+          value={ownerId.toString()}
+          onChangeValue={(value) => setOwnerId(Number(value))}
+          placeholder={'Order Id'}
+        />
+        <TestInputText
+          label={'카테고리'}
+          value={'ALL'}
+          onChangeValue={setCategory}
+          placeholder={'카테고리'}
+        />
+        <TestInputText
+          label={'Size '}
+          value={size.toString()}
+          onChangeValue={(value) => setSize(Number(value))}
+          placeholder={'개수'}
+        />
+      </View>
+      <FreshFlatList<Board>
+        ref={freshFlatListRef}
+        devMode={__DEV__}
+        isFocused={isFocused}
+        fetchList={async (fetchInputMeta) => {
+          const { fetchPage, fetchType, previousList } = fetchInputMeta;
+
+          devLog('#fetchInputMeta:', {
+            fetchPage,
+            fetchType,
+            previousListLength: previousList.length,
+          });
+
+          let response;
+          try {
+            response = await fetchWithTimeout(
+              `${config.api}boards?contentType=BOARD&ownerId=${ownerId}&category=${category}&page=${fetchPage}&size=${size}&sort=createdAt`
+            );
+          } catch (e) {
+            response = new Response(null, {
+              status: 408,
+              statusText: 'Request Timeout',
+            });
+          }
+          devLog('#response.status', response.status);
+          if (response.status !== 200) {
+            devLog(`#fetch status :${response.status}:`, response.statusText);
+          }
+
+          const data: {
+            boardList: Array<Board>;
+            isLast: boolean;
+            isFirst: boolean;
+          } = await response.json();
+
+          let list: Board[] = [];
+          if (data && data.boardList && data.boardList.length > 0) {
+            list = data.boardList;
+          }
+          devLog('#fetch list:', list.length);
+          devLog('#isLast:', data.isLast);
+          return {
+            list: list as Board[],
+            isLastPage: data.isLast,
+          };
+        }}
+        renderItem={({ item, index }: { item: Board; index: number }) => {
+          return (
+            <Pressable
+              style={{ backgroundColor: 'gray', gap: 8, padding: 12 }}
+              onPress={() => goToDetailScreen(item)}
+            >
+              <View style={{ flexDirection: 'row' }}>
+                <Image
+                  source={{ uri: item.writerImage }}
+                  style={{ width: 50, height: 50, borderRadius: 25 }}
+                />
+                <View style={{ paddingLeft: 8 }}>
+                  <Text>{item.writerSpaceName}</Text>
+                  <Text>@{item.writerHandle}</Text>
+                </View>
+              </View>
+
+              <View
+                style={{
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                }}
+              >
+                <View>
+                  <Text style={{ fontWeight: 'bold' }}>index : {index}</Text>
+                  <Text>{item.content}</Text>
+                </View>
+
+                {item.imageList && item.imageList.length > 0 && (
+                  <Image
+                    source={{ uri: item.imageList[0] }}
+                    style={{ width: 75, height: 75, borderRadius: 8 }}
+                  />
+                )}
+              </View>
+
+              <View>
+                <Button
+                  title={'좋아요!'}
+                  onPress={() =>
+                    freshFlatListRef.current?.refreshWatching(index)
+                  }
+                />
+              </View>
+            </Pressable>
+          );
+        }}
+        style={{ flex: 1 }}
+        contentContainerStyle={{
+          flexGrow: 1,
+          paddingHorizontal: 16,
+          gap: 12,
+        }}
+      />
+    </SafeAreaView>
+  );
+}
+ListScreen.display = 'ListScreen';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  box: {
+    width: 50,
+    height: 50,
+    marginVertical: 20,
+    backgroundColor: 'red',
+    flexDirection: 'row',
+    alignSelf: 'center',
+  },
+});
--- src/index.tsx
+++ src/index.tsx
@@ -9,26 +9,20 @@ import {
   useState,
 } from 'react';
 import { useDevLog } from './hooks/useDevLog';
+import type { ViewToken } from '@react-native/virtualized-lists';
 
-// 남득할만한 interface
-// 내 기계는 알아서 딱, 센스 있게 딱!
-
-// 입력 받을 수 있는 props는 오직 하나
-// fetchList: (currentPage) => Promise<number>;
-// initData: 초기에 fetch 없이 list를 보여주고 싶을 때
-
-// RN 타입 중 export 안 되어 있는 타입만 여기에 나열하자
+// List of RN types that are not exported from the RN package
 type onEndReachedParam = { distanceFromEnd: number };
+type onViewableItemsChangedParam<T> = {
+  viewableItems: Array<ViewToken<T>>;
+  changed: Array<ViewToken<T>>;
+};
 
-// 상수 목록
+// list of constants
 const FIRST_PAGE = 1;
-/**
- * FreshTrigger
- * - general: 일반적인 상황에서 fetch를 호출한다.
- * - viewed-page: 페이지 이동 후 되돌아 왔을 때에도 fetch하여 해당 페이지를 갱신한다
- */
-type FreshTrigger = 'general' | 'viewed-page' | string;
-export type FetchType = 'first' | 'current' | 'end-reached';
+
+// Type of FreshFlatList
+export type FetchType = 'first' | 'watching' | 'end-reached';
 export type FetchInputMeta<T> = {
   fetchType: FetchType;
   fetchPage: number;
@@ -39,31 +33,95 @@ export type FetchOutputMeta<T> = Promise<{
   isLastPage: boolean;
 }>;
 
-interface FreshFlatListProps<T> extends Omit<FlatListProps<T>, 'data'> {
-  freshTriggers?: FreshTrigger[];
+interface FreshFlatListProps<T>
+  extends Omit<FlatListProps<T>, 'data' | 'onEndReached' | 'keyExtractor'> {
   isFocused?: boolean;
   fetchList: (fetchInputMeta: FetchInputMeta<T>) => FetchOutputMeta<T>;
   devMode?: boolean;
 }
 export interface FreshFlatListRef {
+  /**
+   * Reset the list to the initial state.
+   */
   reset: () => void;
+  /**
+   * Refresh the current page of the list.
+   * @param index If the index is given, the page containing the index is refreshed. If not, the current page is refreshed.
+   */
+  refreshWatching: (index: number) => void;
 }
 
+// Define FreshFlatList
 function FreshFlatList<T>(
   props: FreshFlatListProps<T>,
   ref: ForwardedRef<FreshFlatListRef>
 ) {
-  const { renderItem, fetchList, devMode, ...otherProps } = props;
+  const {
+    renderItem,
+    fetchList,
+    devMode,
+    isFocused = null,
+    onEndReachedThreshold = 1,
+    ...otherProps
+  } = props;
 
   const [data, setData] = useState<T[]>([]);
+
+  const cache = useRef<Map<number, { data: T[]; timestamp: string }>>(
+    new Map()
+  ).current;
   const previousListRef = useRef(data);
+  const recentlyFetchLastEdgePageRef = useRef(FIRST_PAGE);
   const currentPageRef = useRef(FIRST_PAGE);
-  const currentFetchTypeRef = useRef<FetchType>('current');
   const currentStopNextFetchRef = useRef(false);
+  const isFirstFetchRef = useRef(true);
 
   const devLog = useDevLog(devMode);
 
-  const updateData = useCallback(
+  devLog('#FreshFlatList | data:', data.length);
+  devLog('#FreshFlatList | isFocused:', isFocused);
+  devLog(
+    '#FreshFlatList | recentlyFetchLastEdgePage:',
+    recentlyFetchLastEdgePageRef.current
+  );
+  devLog('#FreshFlatList | currentPage:', currentPageRef.current);
+
+  const keyExtractor = useCallback(
+    (_item: T, index: number) => {
+      let itemIndex = index;
+      let pageIndex = 1;
+      let timestamp = '';
+
+      for (const [page, pageData] of cache.entries()) {
+        if (itemIndex < pageData.data.length) {
+          pageIndex = page;
+          timestamp = pageData.timestamp;
+          break;
+        }
+        itemIndex -= pageData.data.length;
+      }
+
+      return `${pageIndex}-${itemIndex}-${timestamp}`;
+    },
+    [cache]
+  );
+
+  const fetchAndCache = useCallback(
+    async (fetchType: FetchType, page: number) => {
+      const { list, isLastPage } = await fetchList({
+        fetchPage: page,
+        fetchType: fetchType,
+        previousList: previousListRef.current,
+      });
+
+      const timestamp = new Date().toISOString();
+      cache.set(page, { data: list, timestamp });
+      return { list, isLastPage };
+    },
+    [cache, fetchList]
+  );
+
+  const joinData = useCallback(
     (fetchData: T[]) => {
       setData((prevState) => {
         const newData = [...prevState, ...fetchData];
@@ -74,32 +132,64 @@ function FreshFlatList<T>(
     },
     [devLog]
   );
+
   const resetData = useCallback(() => {
     setData([]);
-    currentPageRef.current = FIRST_PAGE;
+    recentlyFetchLastEdgePageRef.current = FIRST_PAGE;
   }, []);
 
+  const getAllCachedData = useCallback(() => {
+    let allData: T[] = [];
+    cache.forEach((pageData) => {
+      allData = [...allData, ...pageData.data];
+    });
+    return allData;
+  }, [cache]);
+
+  const refreshDataFromCache = useCallback(() => {
+    setData(getAllCachedData());
+  }, [getAllCachedData]);
+
+  const refreshWatchingList = useCallback(
+    async (index?: number) => {
+      let pageFromIndex = 0;
+      if (index) {
+        let itemCount = 0;
+        for (const [page, items] of cache.entries()) {
+          itemCount += items.data.length;
+          if (index < itemCount) {
+            pageFromIndex = page;
+            break;
+          }
+        }
+      }
+      const fetchPage = index ? pageFromIndex : currentPageRef.current;
+
+      await fetchAndCache('watching', fetchPage);
+      refreshDataFromCache();
+    },
+    [cache, fetchAndCache, refreshDataFromCache]
+  );
+
+  // Methods that can be controlled from outside the component
   useImperativeHandle(ref, () => ({
     reset: () => {
       resetData();
     },
+    refreshWatching: refreshWatchingList,
   }));
 
   // initial fetch
   useEffect(() => {
-    (async () => {
-      devLog('initial fetch | reset and initial fetch');
-      updateData([]);
-      currentPageRef.current = FIRST_PAGE;
+    if (isFocused === false) return;
+    if (!isFirstFetchRef.current) return; // 값 변경은 "fresh current page when new screen focused"에서 처리
 
-      const { list } = await fetchList({
-        fetchPage: currentPageRef.current,
-        fetchType: currentFetchTypeRef.current,
-        previousList: previousListRef.current,
-      });
-      updateData(list);
+    (async () => {
+      devLog('initial fetch');
+      const { list } = await fetchAndCache('first', FIRST_PAGE);
+      joinData(list);
     })();
-  }, [devLog, fetchList, updateData]);
+  }, [devLog, fetchAndCache, fetchList, isFocused, joinData]);
 
   // fetch when end reached
   const handleOnEndReached = async ({ distanceFromEnd }: onEndReachedParam) => {
@@ -115,26 +205,72 @@ function FreshFlatList<T>(
 
     devLog('#start fetch in onEndReached');
 
-    currentPageRef.current += 1;
-    currentFetchTypeRef.current = 'end-reached';
-    const { list, isLastPage } = await fetchList({
-      fetchPage: currentPageRef.current,
-      fetchType: currentFetchTypeRef.current,
-      previousList: previousListRef.current,
-    });
+    recentlyFetchLastEdgePageRef.current += 1;
+    const { list, isLastPage } = await fetchAndCache(
+      'end-reached',
+      recentlyFetchLastEdgePageRef.current
+    );
     if (isLastPage) currentStopNextFetchRef.current = true;
-    updateData(list);
+    joinData(list);
   };
+
+  // monitor current page
+  const handleOnViewableItemsChanged = useCallback(
+    ({ viewableItems }: onViewableItemsChangedParam<T>) => {
+      if (!viewableItems || viewableItems.length === 0 || !viewableItems[0])
+        return;
+
+      // Get the index of the first visible item
+      const firstVisibleItemIndex = viewableItems[0].index;
+
+      if (firstVisibleItemIndex) {
+        let itemCount = 0;
+
+        // Iterate through the cache to find the current page
+        for (const [page, items] of cache.entries()) {
+          itemCount += items.data.length;
+          if (firstVisibleItemIndex < itemCount) {
+            currentPageRef.current = page;
+            break;
+          }
+        }
+      }
+    },
+    [cache]
+  );
+
+  // fresh current page when new screen focused, Ignore first screen focused
+  useEffect(() => {
+    if (isFocused === null) return;
+    if (isFirstFetchRef.current) {
+      isFirstFetchRef.current = false;
+      return;
+    }
+
+    if (isFocused) {
+      devLog('#fetch when screen focused');
+      refreshWatchingList();
+    }
+  }, [
+    devLog,
+    fetchAndCache,
+    fetchList,
+    isFocused,
+    joinData,
+    refreshDataFromCache,
+    refreshWatchingList,
+  ]);
 
   devLog('#render in FreshFlatList');
 
   return (
     <FlatList<T>
+      keyExtractor={keyExtractor}
       data={data}
       renderItem={renderItem}
-      onEndReachedThreshold={1}
+      onEndReachedThreshold={onEndReachedThreshold}
       onEndReached={handleOnEndReached}
-      contentContainerStyle={{ flexGrow: 1 }}
+      onViewableItemsChanged={handleOnViewableItemsChanged}
       {...otherProps}
     />
   );
@@ -144,10 +280,4 @@ export default forwardRef(FreshFlatList) as <T>(
   props: FreshFlatListProps<T> & { ref?: ForwardedRef<FreshFlatListRef> }
 ) => ReturnType<typeof FreshFlatList>;
 
-/*
-페이지 기반 useCase 나열하자
-- 스크롤 끝에 도달하여 다음 페이지를 호출하고 렌더링 해야한다.
-- 스크롤 최상단에서 당겨서 내리면 list를 다 지우고 새로운 1페이잔 렌더링 한다.
-- 리스트 항목의 상세로 이동하였다가 다시 리스트로 돌아오면, 현재 페이지만 fetch하고 리스트를 렌더링 한다.
- */
 export { useDevLog } from './hooks/useDevLog';
