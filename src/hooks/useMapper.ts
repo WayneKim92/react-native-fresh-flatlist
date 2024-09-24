c9065e536f2621dedc63e6da4b659dad32a2be8b
WayneKim92 <eunchan0817@gmail.com>
2024-09-24 15:30:53 +0900

    chore: 디버깅하기 쉽게 getAll 메소드 추가

--- src/hooks/useMapper.ts
+++ src/hooks/useMapper.ts
@@ -7,7 +7,8 @@ export const usePageKeyMapper = <T>() => {
     set: (page: number, key: T) => {
       map.set(page, key);
     },
-    get: (page: number) => map.get(page),
+    get: (page: number) => map.get(page) as T | undefined,
+    getAll: () => Array.from(map.entries()) as T[] | undefined,
     clear: () => map.clear(),
   };
 };
